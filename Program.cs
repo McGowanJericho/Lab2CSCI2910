8e3180f76d18f5401471951701d7bd5eea9cb5b9
McGowanJ <113394897+McGowanJericho@users.noreply.github.com>
2023-09-18 20:03:49 -0400

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,103 @@
+﻿using Lab_1_Video_Games;
+
+namespace Lab_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int gameCount = 0;
+
+            Dictionary<string, List<VideoGames>> platformDictionary = new Dictionary<string, List<VideoGames>>();
+            using (StreamReader rdr = new StreamReader("../../../Video Game Data/videogames.csv"))
+            {
+                //Platform linked to All games that have that platform.
+                #region 1. Filling and Reading of Games List
+                rdr.ReadLine(); //Allows the skipping of the first line
+                Console.WriteLine("Name | Platform | Year | Genre | Publisher | NA_Sales | EU_Sales | JP_Sales | Other_Sales | Global_Sales");
+                while (rdr.Peek() != -1)
+                {
+
+                    string line = rdr.ReadLine();
+                    string[] info = line.Split(",");
+                    int year = Convert.ToInt32(info[2]);
+                    double na_Sales = Convert.ToDouble(info[5]);
+                    double eu_Sales = Convert.ToDouble(info[6]);
+                    double jp_Sales = Convert.ToDouble(info[7]);
+                    double other_Sales = Convert.ToDouble(info[8]);
+                    double global_Sales = Convert.ToDouble(info[9]);
+                    // public VideoGames(string name, string platform, int year, string genre, string publisher, double na_Sales, double eu_Sales, double jp_Sales, double other_Sales, double global_Sales)
+                    VideoGames game = new VideoGames(info[0], info[1], year, info[3], info[4], na_Sales, eu_Sales, jp_Sales, other_Sales, global_Sales);
+                    string platform = info[1];
+
+
+                    if (platformDictionary.ContainsKey(platform))
+                    {
+                        platformDictionary[platform].Add(game);
+                    }
+                    else
+                    {
+                        List<VideoGames> gamesPlatform = new List<VideoGames> { game };
+                        platformDictionary.Add(platform, gamesPlatform);
+                    }
+                    gameCount++;
+                }
+
+                #endregion
+
+                #region 3. Create a Lambda Function that returns the top 5 video games
+                Func<string, List<VideoGames>> getTop5Games = platform =>
+                {
+                    if (platformDictionary.ContainsKey(platform))
+                    {
+                        List<VideoGames> topGamesList = platformDictionary[platform].OrderByDescending(game => game.Global_Sales).ToList();
+                        List<VideoGames> top5GamesList = new List<VideoGames>();
+                        try //Try catch just in case IndexOutofRange occurs
+                        {
+                            for (int i = 0; i < 5; i++)
+                            {
+                                top5GamesList.Add(topGamesList[i]);
+                            }
+                        }
+                        catch
+                        {
+                            //Do nothing if exception is thrown because IndexOutOfRange
+                        }
+
+                        return top5GamesList;
+                    }
+                    else //If it does not have any items in with that key return an empty list.
+                    {
+                        return new List<VideoGames>();
+                    }
+                };
+
+                #endregion
+
+                #region 4. Display Games:
+                foreach (var platform in platformDictionary.Keys) //For each platform in the dictionary, display the top 5 guys
+                {
+                    List<VideoGames> top5BySales = getTop5Games(platform);
+
+                    if (top5BySales.Count > 0) //If it does not have any games in the list, go to else statement
+                    {
+                        Console.WriteLine($"\nTop {top5BySales.Count} Global Sales for {platform}\n");
+                        for (int i = 0; i < top5BySales.Count; i++) //Display each item in the list
+                        {
+                            VideoGames game = top5BySales[i];
+                            Console.WriteLine($"{game.ToString()}");
+                        }
+                    }
+                    else
+                    {
+                        Console.WriteLine($"No games were found to sort {platform}");
+                    }
+
+
+                    #endregion
+
+                }
+            }
+        }
+    }
+}
---
+++ VideoGames.cs
@@ -0,0 +1,84 @@
+﻿/**
+
+*--------------------------------------------------------------------
+
+* File name: VideoGames.cs
+
+* Project name: Lab 1 Review Lab
+
+*--------------------------------------------------------------------
+
+* Author’s name and email: Jericho McGowan || mcgowanj2@etsu.edu
+
+* Course-Section: CSCI-2910-001
+
+* Creation Date: 9/1/2023
+
+* -------------------------------------------------------------------
+
+*/
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.Design;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_1_Video_Games
+{
+    internal class VideoGames : IComparable<VideoGames>
+    {
+        //Name,Platform,Year,Genre,Publisher,NA_Sales,EU_Sales,JP_Sales,Other_Sales,Global_Sales
+        public string Name { get; set; }
+
+        public string Platform { get; set; }
+        public int Year { get; set; }
+        public string Genre { get; set; }
+        public string Publisher { get; set; }
+        public double NA_Sales { get; set; }
+        public double EU_Sales { get; set; }
+        public double JP_Sales { get; set; }
+        public double Other_Sales { get; set; }
+        public double Global_Sales { get; set; }
+
+        public VideoGames()
+        {
+            this.Name = "N/A";
+            this.Platform = "N/A";
+            this.Year = 2000;
+            this.Genre = "N/A";
+            this.Publisher = "N/A";
+            this.NA_Sales = 0;
+            this.EU_Sales = 0;
+            this.JP_Sales = 0;
+            this.Other_Sales = 0;
+            this.Global_Sales = 0;
+        }
+        public VideoGames(string name, string platform, int year, string genre, string publisher, double na_Sales, double eu_Sales, double jp_Sales, double other_Sales, double global_Sales)
+        {
+            Name = name;
+            Platform = platform;
+            Year = year;
+            Genre = genre;
+            Publisher = publisher;
+            NA_Sales = na_Sales;
+            EU_Sales = eu_Sales;
+            JP_Sales = jp_Sales;
+            Other_Sales = other_Sales;
+            Global_Sales = global_Sales;
+        }
+
+        public int CompareTo(VideoGames? other)
+        {
+            if (other == null) return 1;
+            int compare = this.Name.CompareTo(other.Name);
+            if(compare != 0) return compare;
+            return compare;
+        }
+
+        public override string ToString()
+        {
+            return $"{this.Name} | {this.Year} | {this.Genre} | {this.Publisher} | {this.NA_Sales} | {this.EU_Sales} | {this.JP_Sales} | {this.Other_Sales} | {this.Global_Sales}\n";
+        }
+    }
+}
